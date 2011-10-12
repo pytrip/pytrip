a58c81120f7992fa6db0789bf83d41177a18cf9e
bassler <bassler@95de0f6a-e9e0-4107-90ad-e73cc35f64ce>
2011-10-12 11:34:53 +0000

    more robust gd2dat

--- trunk/pytrip/gd2dat.py
+++ trunk/pytrip/gd2dat.py
@@ -5,34 +5,54 @@
 
 import sys
 import os
+import glob
+
+def is_float(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 class ReadGd(object):
     'read file'
 
-    def __init__(self, filename):
+    def __init__(self, fn):
+        fn_list = glob.glob(fn)
+        for filename in fn_list:
+            if os.path.isfile(filename) is False:
+                raise IOError,  "Could not find file " + filename
+            print filename
+            gd_file = open(filename, 'r')
+            base,ext = os.path.splitext(filename)
+            out_file =open(base+'.dat','w')
 
-        if os.path.isfile(filename) is False:
-            raise IOError,  "Could not find file " + filename
-        
+            gd_lines = gd_file.readlines()
+            gd_file.close()
+            first = True
+            ignore_rest = False
+            for line in gd_lines:
+                #if not(line[0].isdigit()):
+                fff = line.split()
+                if len(fff) > 0:
+                    if not(is_float(fff[0])):
+                        string = "#" + line
+                        if first == False:
+                            ignore_rest = True
+                    else:
+                        first = False
+                        if ignore_rest:
+                            string = "#" + line
+                        else:
+                            string = line
 
-        gd_file = open(filename, 'r')
-        gd_lines = gd_file.readlines()
-        gd_file.close()
-        first = True
-        ignore_rest = False
-        for line in gd_lines:
-            if not(line[0].isdigit()):
-                string = "#" + line
-                if first == False:
-                    ignore_rest = True
-            else:
-                first = False
-                if ignore_rest:
-                    string = "#" + line
-                else:
-                    string = line
+                #sys.stdout.write(string)
+                out_file.write(string)
+            out_file.close()
+            gd_file.close()
 
-            sys.stdout.write(string)
+
+
 
 if __name__ == '__main__':
 
