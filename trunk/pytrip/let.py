5c5644b5df2d8a75d5a4cc3b12ab1302ec0b2677
jtoftegaard <jtoftegaard@95de0f6a-e9e0-4107-90ad-e73cc35f64ce>
2012-08-15 08:59:16 +0000

    added LETCube file

---
+++ trunk/pytrip/let.py
@@ -0,0 +1,13 @@
+import numpy
+import numpy as np
+from error import *
+from cube import *
+
+__author__ = "Niels Bassler and Jakob Toftegaard"
+__version__ = "1.0"
+__email__ = "bassler@phys.au.dk"
+
+class LetCube(Cube):
+        def __init__(self,cube = None):
+                super(LetCube,self).__init__(cube)
+        def 
