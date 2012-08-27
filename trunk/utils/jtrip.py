ec59948d5884cd7df3be30668689d2565bc52217
jtoftegaard <jtoftegaard@95de0f6a-e9e0-4107-90ad-e73cc35f64ce>
2012-08-27 07:00:19 +0000

    Updated utils script

--- trunk/utils/jtrip.py
+++ trunk/utils/jtrip.py
@@ -1,25 +1,40 @@
 import sys
-
-action = sys.argv[1]
-if action is ["add","div","sub","mul"]:
-        if is_number(argv[2]) is True:
-                a = float(argv[2])
-        else:
-                a = Cube()
-                a.read_trip_data_file(argv[2])
-        if is_number(argv[3]) is True:
-                b = float(argv[3])
-        else:
-                b = Cube()
-                b.read_trip_data_file(argv[3])
-        if action is "add":
-                
-
-
+from pytrip.cube import Cube
 def is_number(s):
         try:
                 float(s)
                 return True
         except ValueError:
                 return False
-        
+   
+action = sys.argv[1]
+output = "cube"
+if "-o" in sys.argv:
+	output = sys.argv[sys.argv.index("-o")+1]
+if action in ["add","div","sub","mul"]:
+	file_a = sys.argv[2]
+	file_b = sys.argv[3]
+        if is_number(file_a) is True:
+                a = float(file_a)
+        else:
+                a = Cube()
+                a.read_trip_data_file(file_a)
+        if is_number(file_b) is True:
+                b = float(file_b)
+        else:
+                b = Cube()
+                b.read_trip_data_file(file_b)
+        if action == "add":
+		c = a+b
+	if action == "sub":
+		c = a-b
+	if action == "div":
+		c = a/b
+	if action == "mul":
+		c = a*b
+	c.write_trip_data(output + ".dos")
+	c.write_trip_header(output + ".hed")
+                
+
+
+     
