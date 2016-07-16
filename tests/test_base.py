81e24ced0d1369ef87b07a17f86ac1f2deaeac42
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-07-16 19:00:09 +0200

    tests reorganized

---
+++ tests/test_base.py
@@ -0,0 +1,21 @@
+import sys
+import os
+
+if sys.version_info >= (3,):
+    import urllib.request as urllib2
+else:
+    import urllib2
+import tarfile
+
+
+def get_files():
+    # get plans from https://neptun.phys.au.dk/~bassler/TRiP/
+    bname = "TST003"
+    fname = bname + ".tar.gz"
+    if not os.path.exists(fname):
+        datafile = urllib2.urlopen("https://neptun.phys.au.dk/~bassler/TRiP/" + fname)
+        with open(fname, 'wb') as output:
+            output.write(datafile.read())
+    if not os.path.exists(bname):
+        tfile = tarfile.open(fname, 'r:gz')
+        tfile.extractall(".")
--- tests/test_ctx.py
+++ tests/test_ctx.py
@@ -14,18 +14,50 @@
     You should have received a copy of the GNU General Public License
     along with libdedx.  If not, see <http://www.gnu.org/licenses/>
 """
-from pytrip.ctx import *
-from pytrip.dos import *
-from pytrip.vdx import *
+import hashlib
+import os
+import tempfile
+import unittest
+
+from pytrip.ctx import CtxCube
+
 import pytrip.dicomhelper as dh
+from tests.test_base import get_files
 
-c = CtxCube()
-# c.read("../../../DicomData/test/test2.ctx")
-# c.write("../../../DicomData/test/test2.ctx")
-#
-# print(c.cube[50][100][100])
-# d=c+5
-#
-# print(d.cube[50][100][100])
 
-pass
+class TestCtx(unittest.TestCase):
+    def setUp(self):
+        get_files()
+        self.cube000 = os.path.join("TST003", "tst003000.ctx")
+
+    def test_read(self):
+        c = CtxCube()
+        c.read(self.cube000)
+
+    def test_write(self):
+        # read original cube and calculate hashsum
+        c = CtxCube()
+        c.read(self.cube000)
+        original_md5 = hashlib.md5(open(self.cube000, 'rb').read()).hexdigest()
+
+        # save cube and calculate hashsum
+        fd, outfile = tempfile.mkstemp()
+        print("outfile", outfile)
+        c.write(outfile)
+        generated_md5 = hashlib.md5(open(outfile + ".ctx", 'rb').read()).hexdigest()
+
+        # compare checksums
+        self.assertEqual(original_md5, generated_md5)
+
+        # TODO compare also header files
+
+    def test_addition(self):
+        # read cube
+        c = CtxCube()
+        c.read(self.cube000)
+        d = c + 5
+        self.assertEqual(c.cube[10][20][30] + 5, d.cube[10][20][30])
+
+
+if __name__ == '__main__':
+    unittest.main()
