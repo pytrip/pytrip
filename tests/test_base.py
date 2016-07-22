a411f432a8e3c0bd73b733016756e8a58c875b42
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-07-22 14:14:29 +0200

    test file embedded

--- tests/test_base.py
+++ tests/test_base.py
@@ -10,13 +10,14 @@ else:
 
 def get_files():
     # get plans from https://neptun.phys.au.dk/~bassler/TRiP/
+    dirname = "tests"
     bname = "TST003"
     fname = bname + ".tar.gz"
-    if not os.path.exists(fname):
-        datafile = urllib2.urlopen(
-            "https://neptun.phys.au.dk/~bassler/TRiP/" + fname)
-        with open(fname, 'wb') as output:
+    fname_path = os.path.join(dirname, fname)
+    if not os.path.exists(fname_path):
+        datafile = urllib2.urlopen("https://neptun.phys.au.dk/~bassler/TRiP/" + fname)
+        with open(fname_path, 'wb') as output:
             output.write(datafile.read())
-    if not os.path.exists(bname):
-        tfile = tarfile.open(fname, 'r:gz')
-        tfile.extractall(".")
+    if not os.path.exists(os.path.join(dirname, bname)):
+        tfile = tarfile.open(fname_path, 'r:gz')
+        tfile.extractall(dirname)
