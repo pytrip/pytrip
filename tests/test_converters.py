3d45fa805c5fd449e9fa2e19cf108093e04c4424
Leszek Grzanka <leszek.grzanka@gmail.com>
2016-09-06 11:23:07 +0200

    tests improved removed argparse import which caused flake8 to complain added checking of generated PNG files, to be sure that tests execute properly

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,14 +1,17 @@
+import imghdr
 import unittest
 import os
 import tempfile
-
-import argparse
+import glob
+import logging
 
 from tests.test_base import get_files
 
 import pytrip.utils.trip2dicom
 import pytrip.utils.dicom2trip
 import pytrip.utils.slicedos
+
+logger = logging.getLogger(__name__)
 
 
 class TestTrip2Dicom(unittest.TestCase):
@@ -34,8 +37,13 @@ class TestDicom2Trip(unittest.TestCase):
 
 class TestDosSlicer(unittest.TestCase):
     def setUp(self):
-        testdir = get_files()
-        self.cube000 = os.path.join(testdir, "tst003000")
+        self.dir_path = os.path.join("tests", "res", "TST003")
+
+        self.ctx = os.path.join(self.dir_path, "tst003000.ctx.gz")
+        logger.info("Loading ctx file " + self.ctx)
+
+        self.dos = os.path.join(self.dir_path, "tst003001.dos.gz")
+        logger.info("Loading dos file " + self.dos)
 
     def test_help(self):
         try:
@@ -55,10 +63,28 @@ class TestDosSlicer(unittest.TestCase):
         except SystemExit as e:
             self.assertEqual(e.code, 2)
 
-    def test_many_shield(self):
-        ctx = os.path.join("tests", "res", "TST003", "tst003000.ctx.gz")
-        dos = os.path.join("tests", "res", "TST003", "tst003001.dos.gz")
-        pytrip.utils.slicedos.main(args=[dos, ctx])
+    def test_convert_all(self):
+        pytrip.utils.slicedos.main(args=[self.dos, self.ctx])
+        output_file_list = glob.glob(os.path.join(self.dir_path, "*.png"))
+
+        logger.info("Checking if number of output files is sufficient")
+        self.assertEqual(len(output_file_list), 300)
+
+        for output_file in output_file_list:
+            logger.info("Checking if " + output_file + " is PNG")
+            self.assertEqual(imghdr.what(output_file), 'png')
+
+    # TODO should be activated once it is possible to save output files to some directory
+    # def test_convert_one(self):
+    #     pytrip.utils.slicedos.main(args=[self.dos, self.ctx, "-f", '5' , "-t", '6'])
+    #     output_file_list = glob.glob(os.path.join(self.dir_path, "*.png"))
+    #
+    #     logger.info("Checking if number of output files is sufficient")
+    #     self.assertEqual(len(output_file_list), 1)
+    #
+    #     for output_file in output_file_list:
+    #         logger.info("Checking if " + output_file + " is PNG")
+    #         self.assertEqual(imghdr.what(output_file), 'png')
 
 
 if __name__ == '__main__':
