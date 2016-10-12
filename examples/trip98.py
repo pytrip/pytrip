7323155632aec6ec3eb895e1c8293fc430635aa5
Leszek Grzanka <grzanka@agh.edu.pl>
2016-10-12 15:17:04 +0200

    new example

--- examples/trip98.py
+++ examples/trip98.py
@@ -1,114 +1,72 @@
-import os
-import sys
+import pytrip as pt
+import pytrip.tripexecuter as pte
 
-# necessary pytrip imports
-import argparse
+# first define some paths and other important paramters
+patient_name = "TST000000"
+ctx_path = "/home/bassler/Projects/shieldhit/res/TST000/TST000000.ctx"
+vdx_path = "/home/bassler/Projects/shieldhit/res/TST000/TST000000.vdx"
+voi_name = "GTV"
+working_directory = "/home/bassler/Projects/shieldhit/res/TST000/"
 
-from pytrip import VdxCube
-from pytrip.ctimage import CTImages
-from pytrip.ctx import CtxCube
-from pytrip.tripexecuter.field import Field
-from pytrip.tripexecuter.rbehandler import RBEHandler
+ddd_dir = "/home/bassler/TRiP98/base/DATA/DDD/12C/RF3MM"
+spc_dir = "/home/bassler/TRiP98/base/DATA/SPC/12C/RF3MM"
+sis_path = "/home/bassler/TRiP98/base/DATA/SIS/12C.sis"
+hlut_path = "/home/bassler/TRiP98/base/DATA/HLUT/19990218.hlut"
+dedx_path = "/home/bassler/TRiP98/base/DATA/DEDX/20040607.dedx"
 
-from pytrip.tripexecuter.tripplan import TripPlan
-from pytrip.tripexecuter.tripexecuter import TripExecuter
+my_couch_angle = 90.0
+my_gantry_angle = 10.0
+my_target_voi = "GTV"  # the name must exist in the .vdx file
+my_projectile = "C"
 
+# load CT cube
+my_ctx = pt.CtxCube()
+my_ctx.read(ctx_path)
 
-from pytrip.tripexecuter.tripplancollection import TripPlanCollection
-from pytrip.tripexecuter.tripvoi import TripVoi
-from pytrip.tripexecuter.voi import Voi
-from pytrip.tripexecuter.voicollection import VoiCollection
+# load VOIs
+my_vdx = pt.VdxCube("", my_ctx)  # my_vdx is the object which will hold all volumes of interest and the meta information
+my_vdx.read(vdx_path)  # load the .vdx file
+print(my_vdx.get_voi_names())  # show us all VOIs found in the .vdx file
 
+# next pick a the proper VOI which we want to plan on
+target_voi_temp = my_vdx.get_voi_by_name(my_target_voi)  # Select the requested VOI from the VdxCube object
+target_voi = pte.Voi("GTV_VOI", target_voi_temp)  # for technical reasons the voi must be cast into a new Voi object
 
-def set_trip98_env_variable(trip_path):
-    if not os.path.exists(trip_path):
-        print('TRiP98 location [%s] not existing, provide correct one'.format(trip_path))
-        return 1
-    else:
-        os.environ['TRIP98'] = trip_path
+# Next, setup a plan. We may initialize it with a new name.
+# The name must be identical to the base name of the file, else we will have crash
+my_plan = pte.TripPlan(name=patient_name)
 
+# set working directory, output will go there
+my_plan.set_working_dir(working_directory)
+my_plan.set_ddd_folder(ddd_dir)
+my_plan.set_spc_folder(spc_dir)
+my_plan.set_dedx_file(dedx_path)
+my_plan.set_hlut_file(hlut_path)
+my_plan.set_sis_file(sis_path)
 
-def set_trip98_path_variable(trip_path):
-    trip_bin_path = os.path.join(trip_path, 'bin', 'TRiP98')
-    if not os.path.exists(trip_bin_path):
-        print('TRiP98 bin directory [%s] not existing'.format(trip_bin_path))
-    path_dirs = os.environ['PATH'].split(':')
-    found = False
-    for directory in path_dirs:
-        if 'TRiP98' in os.listdir(directory):
-            found = True
-    if not found:
-        os.environ['PATH'] += ":{:s}".format(os.path.join(trip_path, 'bin'))
+# Remote access currently broken, see issue #94
+# https://github.com/pytrip/pytrip/issues/94
+# my_plan.set_remote_state(True)
+# my_plan.set_server("titan.phys.au.dk")
+# my_plan.set_username("xxxxxxxxx")
+# my_plan.set_password("xxxxxxxxx")
 
+# add target VOI to the plan
+my_plan.add_voi(target_voi)
+my_plan.get_vois()[0].target = True  # make TRiP98 aware of that this VOI is the target.
 
-def main(args=sys.argv[1:]):
+# Finally we need to add a field to the plan
+# add default field, carbon ions
+my_field = pte.Field("Field 1")
+my_field.set_projectile(my_projectile)  # set the projectile
+my_field.set_couch(my_couch_angle)
+my_field.set_gantry(my_gantry_angle)
 
-    description = """
-    description
-    """
+my_plan.add_field(my_field)
 
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("input_path", help='input path', type=str)
-    parser.add_argument("working_directory", help='working directory', type=str)
-    parser.add_argument("--trip", help='directory containing TRiP98 installation', type=str)
-    parsed_args = parser.parse_args(args)
+# the next line is needed to correctly set offset between VOI and CT
+ct_images = pt.CTImages(my_ctx)
 
-    input_path = parsed_args.input_path
-    header_filename, data_filename = CtxCube.parse_path(input_path)
-    if header_filename is None or data_filename is None:
-        print("input path %s doesn't seem to be correct".format(input_path))
-        return 1
-
-    working_directory = parsed_args.working_directory
-    if not os.path.exists(working_directory):
-        print("working directory %s doesn't exists".format(working_directory))
-        return 1
-
-    if 'TRIP98' not in os.environ:
-        if not parsed_args.trip:
-            print('TRiP98 location unknown, provide --trip option')
-            return 1
-        else:
-            set_trip98_env_variable(parsed_args.trip)
-
-    if parsed_args.trip:
-        set_trip98_path_variable(parsed_args.trip)
-
-    patient_name = os.path.split(input_path)[-1]  # basename of CT file is patient_name
-
-    # load CT cube
-    c = CtxCube()
-    c.read(input_path)
-
-    # load VOIs
-    structures = VdxCube("", c)
-    structures.read(input_path + ".vdx")
-    print(structures.get_voi_names())
-    v = structures.get_voi_by_name('GTV')
-    target_voi = Voi("GTV_VOI", v)
-
-    # default plan
-    plan = TripPlan(name=patient_name)
-
-    # set working directory, output will go there
-    plan.set_working_dir(working_directory)
-
-    # add target VOI
-    plan.add_voi(target_voi)
-    plan.get_vois()[0].target = True
-
-    # add default field, carbon ions
-    field = Field("Field 1")
-    field.set_projectile("C")
-    plan.add_field(field)
-
-    # needed to correctly set offset between VOI and CT
-    ct_images = CTImages(c)
-
-    # run TRiP98 optimisation
-    executer = TripExecuter(ct_images.get_modified_images(plan))
-    executer.execute(plan)
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+# run TRiP98 optimisation
+my_trip = pte.TripExecuter(ct_images.get_modified_images(my_plan))
+my_trip.execute(my_plan)
