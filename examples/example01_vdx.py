379c4009b08edd815b2917395d3251b94b8ab0a8
Leszek Grzanka <grzanka@agh.edu.pl>
2016-10-17 20:50:12 +0200

    improved comments (based on discussion in PR #134) and cleaned the code

--- examples/example01_vdx.py
+++ examples/example01_vdx.py
@@ -1,8 +1,6 @@
 import pytrip as pt
-from pytrip.utils import cubeslice
 
 # first define some paths and other important parameters
-patient_name = "TST000000"
 ctx_path = "/home/bassler/Projects/CTdata/TST000/TST000000.ctx"
 vdx_path = "/home/bassler/Projects/CTdata/TST000/TST000000.vdx"
 my_target_voi = "GTV"
@@ -16,23 +14,21 @@ my_vdx = pt.VdxCube("", my_ctx)  # my_vdx is the object which will hold all volu
 my_vdx.read(vdx_path)  # load the .vdx file
 print(my_vdx.get_voi_names())  # show us all VOIs found in the .vdx file
 
-# next pick a the proper VOI which we want to plan on
-target_voi = my_vdx.get_voi_by_name(my_target_voi)  # Select the requested VOI from the VdxCube object
+# Select the requested VOI from the VdxCube object
+target_voi = my_vdx.get_voi_by_name(my_target_voi)
 
-# dos cube representing VOI, it is filled with zeros outside the volume and with 1000 inside
+# get_voi_cube() returns a DosCube() object, where all voxels inside the VOI holds the value 1000, and 0 elsewhere.
 voi_cube = target_voi.get_voi_cube()
 
-# mask object, same shape as dos and voi cube, true inside volume, false outside
+# Based on the retrieved DosCube() we calculate a three dimensional mask object,
+# which assigns True to all voxels inside the Voi, and False elsewhere.
 mask = (voi_cube.cube == 1000)
 
-# make a masked CT - set Hounsfield units to zeros inside the volume
+# "The mask object and the CTX cube have same dimensions (they are infact inherited from the same top level class).
+# Therefore we can apply the mask cube to the ctx cube and work with the values.
+# For instance we can set all HUs to zero within the Voi:
 my_ctx.cube[mask] = 0
 
 # save masked CT to the file in current directory
 masked_ctx = "masked.ctx"
 my_ctx.write(masked_ctx)
-
-# optionally save one slice from original and masked CTs to the png images in current directory
-# it is ugly usage of command-line cubeslice tool, we will improve it with time
-cubeslice.main(["--ct", ctx_path, "-f", "49", "-t", "50", "-o", "."])
-cubeslice.main(["--ct", masked_ctx, "-f", "49", "-t", "50", "-o", "."])
