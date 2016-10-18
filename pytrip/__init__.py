d9868caacfd1d267c42698ba534112a3f814047f
Niels Bassler <niels.bassler@fysik.su.se>
2016-10-18 15:46:56 +0200

    Get rid of old GetDvh class. DVHs are handled byt dos.py

--- pytrip/__init__.py
+++ pytrip/__init__.py
@@ -26,13 +26,12 @@ from pytrip.vdx import VdxCube, Voi
 from pytrip.paths import DensityCube
 from pytrip.raster import Rst
 from pytrip.let import LETCube
-from pytrip.get_dvh import GetDvh
 from pytrip.ctimage import CTImages
 
 # from https://docs.python.org/3/tutorial/modules.html
 # if a package's __init__.py code defines a list named __all__,
 # it is taken to be the list of module names that should be imported when from package import * is encountered.
-__all__ = ['CtxCube', 'VdxCube', 'Voi', 'GetDvh', 'DosCube', 'DensityCube', 'LETCube', 'dicomhelper', 'res',
+__all__ = ['CtxCube', 'VdxCube', 'Voi', 'DosCube', 'DensityCube', 'LETCube', 'dicomhelper', 'res',
            'Rst', 'CTImages']
 
 # if an application using pytrip doesn't configure any logging level, then an error will occur
