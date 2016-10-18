908498e50a35c754a780d3df944702e95ba3e183
Niels Bassler <niels.bassler@fysik.su.se>
2016-10-18 11:57:25 +0200

    Add docstring to file_parser.py

--- pytrip/file_parser.py
+++ pytrip/file_parser.py
@@ -17,6 +17,12 @@
 
 
 def parse_to_var(data, var, stoptag=""):
+    """ Parses a variable 'var' from 'data'.
+
+    :returns: (out,i) tuple
+    out - the rest of the line with 'var' and a " " removed. If nothing is followed, then True is returned.
+    i - number of lines parsed.
+    """
     out = {}
     i = 0
     if type(data) is str:
@@ -25,10 +31,12 @@ def parse_to_var(data, var, stoptag=""):
         if line.find(stoptag) > -1:
             break
         items = line.split()
-        if items[0] in var:
+        if items[0] in var:  # if we have found 'var' in this line..
             if len(items) > 1:
+                # remove "var" from that line, and returns the rest of that line.
                 out[var[items[0]]] = line.replace(items[0] + " ", "")
             else:
+                # in case nothing followed var. The presences of 'var' means smth is set to True.
                 out[var[items[0]]] = True
         i += 1
     return out, i
