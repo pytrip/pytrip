9d443a5b61e4372377d45854123d2755a5e36e03
Niels Bassler <niels.bassler@fysik.su.se>
2016-10-20 12:15:33 +0200

    Fix headers of utils/.

--- pytrip/utils/gd2dat.py
+++ pytrip/utils/gd2dat.py
@@ -1,26 +1,43 @@
 #! /usr/bin/env python
+#
+#    Copyright (C) 2010-2016 PyTRiP98 Developers.
+#
+#    This file is part of PyTRiP98.
+#
+#    PyTRiP98 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PyTRiP98 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
+#
 """ Reads .gd files and can convert them into (xmgrace readable) ascii data.
 
-    Can be used in the command line
+Can be used in the command line
 
-    Mandatory arguments:
-    Name      Type       Default  Description
-    filename  STRING     ""       File name of .gd file, should be first.
+Mandatory arguments:
+Name      Type       Default  Description
+filename  STRING     ""       File name of .gd file, should be first.
 
-    Optional arguments:
-    Name      Type       Default  Description
-    exp       STRING     "exp"    if "exp": Export data of .gd into a .dat file
-    agr       STRING     ""       if "agr": Export data of .gd into a .agr file
-    LET       STRING     "LET"    if "LET": Export also data entitled "DoseLET"
+Optional arguments:
+Name      Type       Default  Description
+exp       STRING     "exp"    if "exp": Export data of .gd into a .dat file
+agr       STRING     ""       if "agr": Export data of .gd into a .agr file
+LET       STRING     "LET"    if "LET": Export also data entitled "DoseLET"
 
-    Example:
-    1) reading the file foo.gd and exporting it as ascii file foo.dat
-    python gd2dat.py foo.gd
+Example:
+1) reading the file foo.gd and exporting it as ascii file foo.dat
+python gd2dat.py foo.gd
 
-    2) reading the file foo.gd and exporting it as xmgrace file foo.agr
-    python gd2dat.py foo.gd agr
+2) reading the file foo.gd and exporting it as xmgrace file foo.agr
+python gd2dat.py foo.gd agr
 """
-
 import sys
 import os
 import logging
