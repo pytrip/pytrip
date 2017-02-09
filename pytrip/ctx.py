fbd46daaf17a6c61dfd1e2e7f57e935c64779a14
Leszek Grzanka <leszek.grzanka@gmail.com>
2017-02-09 13:45:18 +0000

    Fixed datatype of AcquisitionNumber

--- pytrip/ctx.py
+++ pytrip/ctx.py
@@ -102,7 +102,7 @@ class CtxCube(Cube):
 
         ds.KVP = ''  # KVP tag 0x0018, 0x0060
 
-        ds.AcquisitionNumber = 1  # AcquisitionNumber tag 0x0020, 0x0012
+        ds.AcquisitionNumber = '1'  # AcquisitionNumber tag 0x0020, 0x0012 (type IS - Integer String) 
 
         for i in range(len(self.cube)):
             _ds = copy.deepcopy(ds)
