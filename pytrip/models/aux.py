33a02fcbf0bedc5b0761c4d1eea84730f5b7e6a9
Niels Bassler <niels.bassler@fysik.su.se>
2017-03-18 14:33:58 +0100

    add sf->rbe function

--- pytrip/models/aux.py
+++ pytrip/models/aux.py
@@ -19,22 +19,38 @@
 """
 Auxilliary tools
 """
-
+import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def rbe_from_sf(sf_ion, dose_ion, ax, bx):
+def rbe_from_sf(sf_ion, dose_ion, alpha_x, beta_x):
     """
     Returns the RBE for given ion survivng fraction and (alpha/beta)x-ray
-    :params dose: ion physical dose in [Gy]
-    :params sf_ion: surviving fraction in ion beam
-    :params ax: alpha for X-rays in [Gy^-1]
-    :params bx: beta for X-rays in [Gy^-2]
+    :params float dose: ion physical dose in [Gy] (cube or scalar)
+    :params float sf_ion: surviving fraction in ion beam (cube of scalar)
+    :params float alpha_x: alpha for X-rays in [Gy^-1] (cube or scalar)
+    :params float beta_x: beta for X-rays in [Gy^-2] (cube or scalar)
     """
 
-    # Solve for D_x: bD2 + aD + ln(sf_ion) = 0
+    # Solve for D_x: beta_x*D*2 + alpha_x*D + ln(sf_ion) = 0
     # RBE = D_x / D_ion
-    logger.warning("rcr_rbe not implemented yet.")
-    pass  # TODO: not implemented yet.
+
+    a = beta_x * dose_ion * 2.0
+    b = alpha_x * dose_ion
+    c = np.log(sf_ion)  # natural logarithm
+    d = b*b - 4*a*c
+    x1 = (-b + np.sqrt(d)) / (2.0*a)
+    x2 = (-b - np.sqrt(d)) / (2.0*a)
+
+    if x1 > x2:
+        dose_x = x1
+    else:
+        dose_x = x2
+
+    rbe = dose_x / dose_ion
+
+    if rbe < 0:
+        logger.warning("negative rbe encountered")
+    return rbe
