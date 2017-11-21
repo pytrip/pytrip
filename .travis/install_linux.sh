ee20e699e9b07d6a1d80117740fe8865dc4f48f7
Leszek Grzanka <leszek.grzanka@gmail.com>
2017-11-21 18:24:40 +0100

    imit py version as starting from 1.5 support for python 3.3 is dropped

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -8,16 +8,14 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
                 # exit with a non-zero status, or zero if all commands in the
                 # pipeline exit successfully.
 
-# check ubuntu version
-lsb_release -a
-
 if [[ $TOXENV == py32* ]];
 then
     sudo apt-get -qq update
     sudo apt-get install --no-install-recommends -y libblas-dev liblapack-dev gfortran libfreetype6-dev g++
 fi
 
-pip install --upgrade virtualenv$VENVVER pip$PIPVER setuptools$STVER tox wheel
+# we limit py version as starting from 1.5 support for python 3.3 is dropped
+pip install --upgrade virtualenv$VENVVER pip$PIPVER setuptools$STVER tox wheel "py<1.5"
 
 pip install -r requirements.txt
 pip install -r tests/requirements-test.txt
