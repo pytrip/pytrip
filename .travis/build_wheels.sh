3e5bf46e0fbf2e5087c1456a2b2b405c0f34375c
Leszek Grzanka <grzanka@agh.edu.pl>
2017-12-30 21:57:07 +0100

    code cleaning

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -3,7 +3,7 @@
 # script based on https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
 set -e -x
 
-# freetype v2.3.0 installation, some versions of matplotlib require it
+# freetype v2.3.0 installation, source versions of matplotlib require it to get installed via pip
 # docker image has only freetype v2.2
 install_freetype() {
     wget --no-check-certificate http://downloads.sourceforge.net/freetype/freetype-2.3.0.tar.gz
@@ -21,6 +21,7 @@ PYVERS=$1
 # tk-devel is needed to run code dependent on matplotlib
 yum install -y tk-devel
 
+# loop over python targets and make pytrip wheel package for each of it
 for TARGET in $PYVERS;
 do
     # Set python location
@@ -39,16 +40,21 @@ do
     cd -
 
     # see https://github.com/google/python-subprocess32/issues/12#issuecomment-337806379
+    # installation of subprocess32 port package from source fails, so let us use
+    # current development version (--pre enables that)
     ${PYBIN}/pip install --pre "subprocess32 ; python_version < '3.0'"
 
     # Install requirements and get the exit code
     # do not include pre-releases (i.e. RC - release candidates) and development versions
+    # forcing use of binary package for matplotlib as source package has API confict with numpy
+    # see PR #419
     set +e
     ${PYBIN}/pip install --upgrade -r /io/requirements.txt --only-binary matplotlib
     RET_CODE=$?
     set -e
 
-    # is normal installation failed, try install freetype and try again
+    # is normal installation failed, try install freetype and try again,
+    # this time with source package for matplotlib
     if [ $RET_CODE -ne 0 ]; then
         install_freetype
         # libpng is needed by matplotlib, blas and lapack by numpy
