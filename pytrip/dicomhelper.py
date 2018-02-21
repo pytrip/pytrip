f3c2fdb1248c014599f528ffd762da03becf2d77
Leszek Grzanka <grzanka@agh.edu.pl>
2018-02-21 12:57:39 +0100

    support for newer (>1.0) version of pydicom package, which require new way of importing

--- pytrip/dicomhelper.py
+++ pytrip/dicomhelper.py
@@ -17,10 +17,13 @@
 #    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
 #
 """
-Auxilliary functions for handling Dicom data.
+Auxiliary functions for handling Dicom data.
 """
 import os
-import dicom
+try:
+    import pydicom as dicom  # as of version 1.0 pydicom package should be used this way
+except ImportError:
+    import dicom  # fallback to old (<1.0) pydicom package version
 
 
 def compare_dicom_key(dcm):
@@ -33,7 +36,7 @@ def read_dicom_dir(dicom_dir):
     """ Reads a directory with dicom files.
     Identifies each dicom file with .dcm suffix and returns a dict containing a dicom object.
     Dicom object may be "CT", "RTSTRUCT", "RTDOSE" or "RTPLAN".
-    Corresponding keys for lookup are "images", "rtss", "rtdose" or "rtplan" repsectively.
+    Corresponding keys for lookup are "images", "rtss", "rtdose" or "rtplan" respectively.
     CT objects are lists of images. They will be sorted by the position in patient given
     by the ImagePositionPatient[2] tag.
 
@@ -42,7 +45,7 @@ def read_dicom_dir(dicom_dir):
     if not os.path.isdir(dicom_dir):
         raise IOError("Directory {:s} does not exist".format(dicom_dir))
 
-    # list of allowed dicom file extentions names
+    # list of allowed dicom file extensions names
     # all in lower case
     dicom_suffix = ('.dcm', '.ima', '.v2')
 
