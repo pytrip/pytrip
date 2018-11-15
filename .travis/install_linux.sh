4e46ef9c25155135fc8b2e1d7fd0e304770161d3
Leszek Grzanka <leszek.grzanka@gmail.com>
2018-11-15 17:42:16 +0100

    Abandoning python 3.2 (#479)

--- .travis/install_linux.sh
+++ .travis/install_linux.sh
@@ -8,14 +8,7 @@ set -o pipefail # Return value of a pipeline as the value of the last command to
                 # exit with a non-zero status, or zero if all commands in the
                 # pipeline exit successfully.
 
-if [[ $TOXENV == py32* ]];
-then
-    sudo apt-get -qq update
-    sudo apt-get install --no-install-recommends -y libblas-dev liblapack-dev gfortran libfreetype6-dev g++
-fi
-
-# we limit py version as starting from 1.5 support for python 3.3 is dropped
-pip install --upgrade virtualenv$VENVVER pip$PIPVER setuptools$STVER tox wheel "py<1.5"
+pip install --upgrade virtualenv pip setuptools tox wheel
 
 pip install -r requirements.txt
 pip install -r tests/requirements-test.txt
