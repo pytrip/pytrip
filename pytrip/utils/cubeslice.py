9816e467a428e6b1395a55558ab901a9252abb99
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-02-10 14:49:23 +0100

    Feature/530 output dir cubeslicer (#531)
    * typos fixed, directory creation implemented
    
    * cleaning logger framework, previous implementation was silent

--- pytrip/utils/cubeslice.py
+++ pytrip/utils/cubeslice.py
@@ -19,17 +19,15 @@
 """
 Generates .png files for each slice found in the given cube.
 """
-import os
-import sys
 import argparse
 import logging
+import os
+import sys
 
 from numpy import arange, ma, NINF
 
 import pytrip as pt
 from pytrip.util import TRiP98FilePath
-
-logger = logging.getLogger(__name__)
 
 
 def load_data_cube(filename):
@@ -40,10 +38,10 @@ def load_data_cube(filename):
     """
 
     if not filename:
-        logger.warn("Empty data cube filename")
+        logging.warning("Empty data cube filename")
         return None, None
 
-    logger.info("Reading " + filename)
+    logging.info("Reading " + filename)
 
     d = None
     basename_cube = None
@@ -56,11 +54,11 @@ def load_data_cube(filename):
     let_path_helper = TRiP98FilePath(filename, pt.LETCube)
     dose_path_helper = TRiP98FilePath(filename, pt.DosCube)
 
-    # check if LET cube type can be determinen by presence of suffix in a name (mlet, dosemlet)
+    # check if LET cube type can be determined by presence of suffix in a name (mlet, dosemlet)
     if let_path_helper.suffix is not None:
         cube_cls = let_path_helper.cube_type
 
-    # check if DOS cube type can be determinen by presence of suffix in a name (phys, bio etc...)
+    # check if DOS cube type can be determined by presence of suffix in a name (phys, bio etc...)
     elif dose_path_helper.suffix is not None:
         cube_cls = dose_path_helper.cube_type
 
@@ -72,15 +70,15 @@ def load_data_cube(filename):
 
     if d is not None:  # parsing successful, we can proceed to reading the file
         d.read(filename)
-        logger.info("Data cube shape" + str(d.cube.shape))
+        logging.info("Data cube shape" + str(d.cube.shape))
         if isinstance(d, pt.DosCube):
             d *= 0.1  # convert %% to %
 
         dmax = d.cube.max()
         dmin = d.cube.min()
-        logger.info("Data min, max values: {:g} {:g}".format(dmin, dmax))
+        logging.info("Data min, max values: {:g} {:g}".format(dmin, dmax))
     else:
-        logger.warning("Filename " + filename + " is neither valid DOS nor LET cube")
+        logging.warning("Filename " + filename + " is neither valid DOS nor LET cube")
 
     basename_cube = d.basename
 
@@ -95,17 +93,17 @@ def load_ct_cube(filename):
     """
 
     if not filename:
-        logger.warning("Empty CT cube filename")
+        logging.warning("Empty CT cube filename")
         return None, None
 
-    logger.info("Reading " + filename)
+    logging.info("Reading " + filename)
     c = pt.CtxCube()
     c.read(filename)
-    logger.info("CT cube shape" + str(c.cube.shape))
+    logging.info("CT cube shape" + str(c.cube.shape))
 
     cmax = c.cube.max()
     cmin = c.cube.min()
-    logger.info("CT min, max values: {:d} {:d}".format(cmin, cmax))
+    logging.info("CT min, max values: {:d} {:d}".format(cmin, cmax))
 
     return c, c.basename
 
@@ -125,51 +123,51 @@ def main(args=sys.argv[1:]):
 
     # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", help="data cube(dos, let etc)", type=str, nargs='?')
+    parser.add_argument("--data", help="data cube(dose, LET etc)", type=str, nargs='?')
     parser.add_argument("--ct", help="CT cube", type=str, nargs='?')
     parser.add_argument(
         "-f", "--from", type=int, dest='sstart', metavar='N', help="Output from slice number N", default=1)
     parser.add_argument("-t", "--to", type=int, dest='sstop', metavar='M', help="Output up to slice number M")
     parser.add_argument("-H", "--HUbar", dest='HUbar', default=False, action='store_true', help="Add HU colour bar")
     parser.add_argument("-m", "--max", type=float, dest='csmax', metavar='csmax',
-                        help="Maximum value of colorscale for plotting data")
+                        help="Maximum colorscale value for plotting data")
     parser.add_argument("-o", "--outputdir", dest='outputdir',
-                        help="Write resulting files to this directory.", type=str, default=None)
+                        help="Directory for output files.", type=str, default=None)
     parser.add_argument('-v', '--verbosity', action='count', help="increase output verbosity", default=0)
     parser.add_argument('-V', '--version', action='version', version=pt.__version__)
-    args = parser.parse_args(args)
+    parsed_args = parser.parse_args(args)
 
-    if args.verbosity == 1:
-        logger.setLevel('INFO')
-    if args.verbosity > 1:
-        logger.setLevel('DEBUG')
+    if parsed_args.verbosity == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif parsed_args.verbosity > 1:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
 
-    if args.data is None and args.ct is None:
-        logger.error("Provide location of at least one cube")
+    if parsed_args.data is None and parsed_args.ct is None:
+        logging.error("Provide location of at least one cube")
         return 2
 
     # Check if different output path was requested. If yes, then check if it exists.
-    if args.outputdir is not None:
-        if os.path.isdir(args.outputdir) is False:
-            logger.error("Output directory " + args.outputdir + " does not exist.")
-            return 1
+    if parsed_args.outputdir:
+        if not os.path.isdir(parsed_args.outputdir):
+            logging.warning("Output directory " + parsed_args.outputdir + " does not exist, creating.")
+            os.makedirs(parsed_args.outputdir)
 
-    data_cube, data_basename = load_data_cube(args.data)
+    data_cube, data_basename = load_data_cube(parsed_args.data)
 
-    ct_cube, ct_basename = load_ct_cube(args.ct)
+    ct_cube, ct_basename = load_ct_cube(parsed_args.ct)
 
     # check cube data are compatible (if both cubes present)
     if data_cube is not None and ct_cube is not None:
         if not data_cube.is_compatible(ct_cube):
-            logger.error("Cubes don't match")
-            logger.info("Cube 1 " + args.data + " shape {:d} x {:d} x {:d}".format(data_cube.dimx, data_cube.dimy,
-                                                                                   data_cube.dimz))
-            logger.info("Cube 1 " + args.data + " pixel {:g} [cm], slice {:g} [cm]".format(data_cube.pixel_size,
-                                                                                           data_cube.slice_distance))
-            logger.info("Cube 2 " + args.ct + " shape {:d} x {:d} x {:d}".format(ct_cube.dimx, ct_cube.dimy,
-                                                                                 ct_cube.dimz))
-            logger.info("Cube 2 " + args.ct + " pixel {:g} [cm], slice {:g} [cm]".format(ct_cube.pixel_size,
-                                                                                         ct_cube.slice_distance))
+            logging.error("Cubes don't match")
+            logging.info("Cube 1 " + parsed_args.data + " : ")
+            logging.info("\tshape {:d} x {:d} x {:d}".format(data_cube.dimx, data_cube.dimy, data_cube.dimz))
+            logging.info("\tpixel {:g} [cm], slice {:g} [cm]".format(data_cube.pixel_size, data_cube.slice_distance))
+            logging.info("Cube 2 " + parsed_args.ct + " : ")
+            logging.info("\tshape {:d} x {:d} x {:d}".format(ct_cube.dimx, ct_cube.dimy, ct_cube.dimz))
+            logging.info("\tpixel {:g} [cm], slice {:g} [cm]".format(ct_cube.pixel_size, ct_cube.slice_distance))
             return 2
 
     cube = None
@@ -181,11 +179,11 @@ def main(args=sys.argv[1:]):
         cube = ct_cube
         cube_basename = ct_basename
     else:
-        logger.error("Both (data and CT) cubes are empty")
+        logging.error("Both (data and CT) cubes are empty")
         return 2
 
     # calculating common frame for printing cubes
-    logger.info("Number of slices: " + str(cube.dimz))
+    logging.info("Number of slices: " + str(cube.dimz))
 
     # get actual X and Y positions of bin centers, for two opposite corners, lowest and highest
     # i.e. assuming cube 5x5x5 with pixel_size 1 and slice_distance 1, we will have:
@@ -194,8 +192,8 @@ def main(args=sys.argv[1:]):
     xmin_center, ymin_center, zmin_center = cube.indices_to_pos([0, 0, 0])
     xmax_center, ymax_center, zmax_center = cube.indices_to_pos([cube.dimx - 1, cube.dimy - 1, cube.dimz - 1])
 
-    logger.info("First bin center: {:10.2f} {:10.2f} {:10.2f} [mm]".format(xmin_center, ymin_center, zmin_center))
-    logger.info("Last bin center : {:10.2f} {:10.2f} {:10.2f} [mm]".format(xmax_center, ymax_center, zmax_center))
+    logging.info("First bin center: {:10.2f} {:10.2f} {:10.2f} [mm]".format(xmin_center, ymin_center, zmin_center))
+    logging.info("Last bin center : {:10.2f} {:10.2f} {:10.2f} [mm]".format(xmax_center, ymax_center, zmax_center))
 
     # get lowest corner of leftmost lowest bin
     xmin_lowest, ymin_lowest = xmin_center - 0.5 * cube.pixel_size, ymin_center - 0.5 * cube.pixel_size
@@ -207,21 +205,21 @@ def main(args=sys.argv[1:]):
     ct_im = None
     data_im = None
 
-    if args.sstart <= 0:
-        logger.error("Invalid slice number {:d}. First slice must be 1 or greater.".format(args.sstart))
+    if parsed_args.sstart <= 0:
+        logging.error("Invalid slice number {:d}. First slice must be 1 or greater.".format(parsed_args.sstart))
         return 1
 
     # if user hasn't provided number of first slice, then argument parsing method will set it to zero
-    slice_start = args.sstart
+    slice_start = parsed_args.sstart
 
     # user hasn't provided number of last slice, we assume then that the last one has number cube.dimz
-    slice_stop = args.sstop
+    slice_stop = parsed_args.sstop
     if slice_stop is None:
         slice_stop = cube.dimz
 
     # user hasn't provided maximum limit of colorscale, we assume then maximum value of data cube, if present
     # we clip data to the maximum value of colorscale
-    data_colorscale_max = args.csmax
+    data_colorscale_max = parsed_args.csmax
     if data_colorscale_max is None and data_cube is not None:
         data_colorscale_max = cube.cube.max()
     data_cube.cube.clip(NINF, data_colorscale_max, data_cube.cube)
@@ -253,13 +251,13 @@ def main(args=sys.argv[1:]):
         output_filename = "{:s}_{:03d}.png".format(cube_basename, slice_id)
 
         slice_str = str(slice_id) + "/" + str(cube.dimz)
-        if args.outputdir is not None:
+        if parsed_args.outputdir is not None:
             # use os.path.join() in order to add slash if it is missing.
-            output_filename = os.path.join(args.outputdir, os.path.basename(output_filename))
-        if args.verbosity == 0:
+            output_filename = os.path.join(parsed_args.outputdir, os.path.basename(output_filename))
+        if parsed_args.verbosity == 0:
             print("Write slice number: " + slice_str)
-        if args.verbosity > 0:
-            logger.info("Write slice number: " + slice_str + " to " + output_filename)
+        elif parsed_args.verbosity > 0:
+            logging.info("Write slice number: " + slice_str + " to " + output_filename)
 
         slice_str = "Slice #: {:3d}/{:3d}\nSlice position: {:6.2f} mm".format(slice_id,
                                                                               cube.dimz,
@@ -286,7 +284,7 @@ def main(args=sys.argv[1:]):
             )
 
             # optionally add HU bar
-            if args.HUbar and ct_cb is None:
+            if parsed_args.HUbar and ct_cb is None:
                 ct_cb = fig.colorbar(ct_im, ax=ax, ticks=arange(-1000, 3000, 200), orientation='horizontal')
                 ct_cb.set_label('HU')
 
@@ -332,12 +330,12 @@ def main(args=sys.argv[1:]):
                 elif isinstance(data_cube, pt.DosCube):
                     data_cb.set_label("Relative dose [%]")
 
-        # by default savefil will produce 800x600 resolution, setting dpi=200 is increasing it to 1600x1200
+        # by default savefig will produce 800x600 resolution, setting dpi=200 is increasing it to 1600x1200
         fig.savefig(output_filename, dpi=200)
 
+    logging.info("Done")
     return 0
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     sys.exit(main(sys.argv[1:]))
