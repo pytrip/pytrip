ccf61fa17a0c7eb0c711eaa3a8328f768d30f40b
Leszek Grzanka <grzanka@agh.edu.pl>
2021-03-14 21:44:05 +0100

    testing python 3.9

--- setup.py
+++ setup.py
@@ -163,6 +163,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython'
     ],
     package_data={'pytrip': ['data/*.dat', 'pytriplib.*', 'cntr.*']},
