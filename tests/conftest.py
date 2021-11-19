d60ae0350f088a8e3c1c48c591e6e7ca4e35f29d
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-11-19 19:42:21 +0100

    package build fix [build]

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,16 @@ def ctx_corename():
     return os.path.join('tests', 'res', 'TST003', 'tst003000')
 
 
+@pytest.fixture(scope='module', params=["", ".ctx", ".hed", ".CTX", ".HED", ".hed.gz", ".ctx.gz"])
+def ctx_allnames(ctx_corename):
+    return ctx_corename
+
+
 @pytest.fixture(scope='module')
 def vdx_filename():
     return os.path.join('tests', 'res', 'TST003', 'tst003000.vdx')
 
 
-@pytest.fixture(scope='module', params=["", ".ctx", ".hed", ".CTX", ".HED", ".hed.gz", ".ctx.gz"])
-def ctx_allnames(ctx_corename):
-    return ctx_corename
+@pytest.fixture(scope='module')
+def rst_filename():
+    return os.path.join('tests', 'res', 'TST003', 'tst003001.rst')
