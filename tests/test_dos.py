eecedb2f2704e0589343f689b4d4d1ff85f69f81
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-11-19 22:43:22 +0100

    more tests ported

--- tests/test_dos.py
+++ tests/test_dos.py
@@ -1,160 +1,156 @@
-# #
-# #    Copyright (C) 2010-2017 PyTRiP98 Developers.
-# #
-# #    This file is part of PyTRiP98.
-# #
-# #    PyTRiP98 is free software: you can redistribute it and/or modify
-# #    it under the terms of the GNU General Public License as published by
-# #    the Free Software Foundation, either version 3 of the License, or
-# #    (at your option) any later version.
-# #
-# #    PyTRiP98 is distributed in the hope that it will be useful,
-# #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-# #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# #    GNU General Public License for more details.
-# #
-# #    You should have received a copy of the GNU General Public License
-# #    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
-# #
-# """
-# TODO: documentation here.
-# """
-# import logging
-# import os
-# import shutil
-# import tempfile
-# import unittest
 #
-# import numpy as np
-# import pytest
+#    Copyright (C) 2010-2017 PyTRiP98 Developers.
 #
-# from pytrip import pytriplib
-# from pytrip.dos import DosCube
-# from pytrip.vdx import create_sphere
-# from pytrip.volhist import VolHist
+#    This file is part of PyTRiP98.
 #
-# import tests.base
+#    PyTRiP98 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# logger = logging.getLogger(__name__)
+#    PyTRiP98 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
+#    You should have received a copy of the GNU General Public License
+#    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
 #
-# class TestDos(unittest.TestCase):
-#     def setUp(self):
-#         testdir = tests.base.get_files()
-#         self.cube000 = os.path.join(testdir, "tst003001.dos")
-#         logger.info("Testing cube from path " + self.cube000)
-#
-#     def test_read(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#         self.assertEqual(c.cube.shape[0], 300)
-#         self.assertEqual(c.cube.shape[1], 512)
-#         self.assertEqual(c.cube.shape[2], 512)
-#
-#         # test method from C extension
-#         dose_center = pytriplib.calculate_dose_center(np.array(c.cube))
-#         self.assertEqual(dose_center.shape[0], 3)
-#         self.assertGreater(dose_center[0], 0.0)
-#         self.assertGreater(dose_center[1], 0.0)
-#         self.assertGreater(dose_center[2], 0.0)
-#
-#     def test_dvh(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#         v = create_sphere(c, name="sph", center=[10, 10, 10], radius=8)
-#         self.assertIsNotNone(v)
-#
-#         logger.info("Calculating DVH")
-#         result = c.calculate_dvh(v)
-#         self.assertIsNotNone(result)
-#         dvh, min_dose, max_dose, mean, area = result
-#         self.assertGreater(area, 2.0)
-#         self.assertEqual(len(dvh.shape), 2)
-#         self.assertEqual(dvh.shape[1], 2)
-#         self.assertEqual(dvh.shape[0], 1500)
-#         self.assertEqual(min_dose, 0.0)
-#         self.assertEqual(max_dose, 0.001)
-#
-#     def test_dvh_simple(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#         v = create_sphere(c, name="sph", center=[10, 10, 10], radius=8)
-#         self.assertIsNotNone(v)
-#
-#         logger.info("Calculating DVH simple")
-#         vh = VolHist(c, v)
-#         self.assertIsNotNone(vh.x)
-#         self.assertIsNotNone(vh.y)
-#
-#         outdir = tempfile.mkdtemp()
-#         c.write_dicom(outdir)
-#
-#         f1 = os.path.join(outdir, "foobar_1.dvh")
-#         vh.write(f1, header=True)
-#         self.assertTrue(os.path.exists(f1))
-#         self.assertGreater(os.path.getsize(f1), 0)
-#
-#         f2 = os.path.join(outdir, "foobar_2.dvh")
-#         vh.write(f2, header=False)
-#         self.assertTrue(os.path.exists(f2))
-#         self.assertGreater(os.path.getsize(f2), 0)
-#
-#         logger.info("Calculating DVH simple for entire cube")
-#         vh = VolHist(c)
-#         self.assertIsNotNone(vh.x)
-#         self.assertIsNotNone(vh.y)
-#
-#         f3 = os.path.join(outdir, "foobar_3.dvh")
-#         vh.write(f3, header=True)
-#         self.assertTrue(os.path.exists(f3))
-#         self.assertGreater(os.path.getsize(f3), 0)
-#
-#         f4 = os.path.join(outdir, "foobar_4.dvh")
-#         vh.write(f4, header=False)
-#         self.assertTrue(os.path.exists(f4))
-#         self.assertGreater(os.path.getsize(f4), 0)
-#
-#         shutil.rmtree(outdir)
-#         # TODO: add some quantitative tests
-#
-#     def test_dicom_plan(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#
-#         dp = c.create_dicom_plan()
-#         self.assertIsNotNone(dp)
-#
-#         d = c.create_dicom()
-#         self.assertIsNotNone(d)
-#
-#     def test_write_dicom(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#
-#         outdir = tempfile.mkdtemp()
-#         c.write_dicom(outdir)
-#         self.assertTrue(os.path.exists(os.path.join(outdir, "rtdose.dcm")))
-#         self.assertTrue(os.path.exists(os.path.join(outdir, "rtplan.dcm")))
-#         self.assertGreater(os.path.getsize(os.path.join(outdir, "rtdose.dcm")), 0)
-#         self.assertGreater(os.path.getsize(os.path.join(outdir, "rtplan.dcm")), 0)
-#         shutil.rmtree(outdir)
-#
-#     @pytest.mark.smoke
-#     def test_write(self):
-#         c = DosCube()
-#         c.read(self.cube000)
-#
-#         fd, outfile = tempfile.mkstemp()
-#         os.close(fd)  # Windows needs it
-#         os.remove(outfile)  # we need only temp filename, not the file
-#         c.write(outfile)
-#         hed_file = outfile + DosCube.header_file_extension
-#         dos_file = outfile + DosCube.data_file_extension
-#         self.assertTrue(os.path.exists(hed_file))
-#         self.assertTrue(os.path.exists(dos_file))
-#         logger.info("Checking if output file " + hed_file + " is not empty")
-#         self.assertGreater(os.path.getsize(hed_file), 1)
-#         logger.info("Checking if output file " + dos_file + " is not empty")
-#         self.assertGreater(os.path.getsize(dos_file), 1)
-#         os.remove(hed_file)
-#         os.remove(dos_file)
+"""
+TODO: documentation here.
+"""
+import logging
+import os
+import shutil
+import tempfile
+
+import numpy as np
+import pytest
+
+from pytrip import pytriplib
+from pytrip.dos import DosCube
+from pytrip.vdx import create_sphere
+from pytrip.volhist import VolHist
+
+logger = logging.getLogger(__name__)
+
+
+def test_read(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+    assert c.cube.shape[0] == 300
+    assert c.cube.shape[1] == 512
+    assert c.cube.shape[2] == 512
+
+    # test method from C extension
+    dose_center = pytriplib.calculate_dose_center(np.array(c.cube))
+    assert dose_center.shape[0] == 3
+    assert dose_center[0] > 0.
+    assert dose_center[1] > 0.
+    assert dose_center[2] > 0.
+
+
+def test_dvh(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+    v = create_sphere(c, name="sph", center=[10, 10, 10], radius=8)
+    assert v is not None
+
+    logger.info("Calculating DVH")
+    result = c.calculate_dvh(v)
+    assert result is not None
+    dvh, min_dose, max_dose, mean, area = result
+    assert area > 2.0
+    assert len(dvh.shape) == 2
+    assert dvh.shape[1] == 2
+    assert dvh.shape[0] == 1500
+    assert min_dose == 0.
+    assert max_dose == 0.001
+
+
+def test_dvh_simple(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+    v = create_sphere(c, name="sph", center=[10, 10, 10], radius=8)
+    assert v is not None
+
+    logger.info("Calculating DVH simple")
+    vh = VolHist(c, v)
+    assert vh.x is not None
+    assert vh.y is not None
+
+    outdir = tempfile.mkdtemp()
+    c.write_dicom(outdir)
+
+    f1 = os.path.join(outdir, "foobar_1.dvh")
+    vh.write(f1, header=True)
+    assert os.path.exists(f1)
+    assert os.path.getsize(f1) > 1
+
+    f2 = os.path.join(outdir, "foobar_2.dvh")
+    vh.write(f2, header=False)
+    assert os.path.exists(f2)
+    assert os.path.getsize(f2) > 1
+
+    logger.info("Calculating DVH simple for entire cube")
+    vh = VolHist(c)
+    assert vh.x is not None
+    assert vh.y is not None
+
+    f3 = os.path.join(outdir, "foobar_3.dvh")
+    vh.write(f3, header=True)
+    assert os.path.exists(f3)
+    assert os.path.getsize(f3) > 1
+
+    f4 = os.path.join(outdir, "foobar_4.dvh")
+    vh.write(f4, header=False)
+    assert os.path.exists(f4)
+    assert os.path.getsize(f4) > 1
+
+    shutil.rmtree(outdir)
+    # TODO: add some quantitative tests
+
+
+def test_dicom_plan(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+
+    dp = c.create_dicom_plan()
+    assert dp is not None
+
+    d = c.create_dicom()
+    assert d is not None
+
+
+def test_write_dicom(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+
+    outdir = tempfile.mkdtemp()
+    c.write_dicom(outdir)
+    assert os.path.exists(os.path.join(outdir, "rtdose.dcm")) is True
+    assert os.path.exists(os.path.join(outdir, "rtplan.dcm")) is True
+    assert os.path.getsize(os.path.join(outdir, "rtdose.dcm")) > 1
+    assert os.path.getsize(os.path.join(outdir, "rtplan.dcm")) > 1
+    shutil.rmtree(outdir)
+
+
+@pytest.mark.smoke
+def test_write(dos_filename):
+    c = DosCube()
+    c.read(dos_filename)
+
+    fd, outfile = tempfile.mkstemp()
+    os.close(fd)  # Windows needs it
+    os.remove(outfile)  # we need only temp filename, not the file
+    c.write(outfile)
+    hed_file = outfile + DosCube.header_file_extension
+    dos_file = outfile + DosCube.data_file_extension
+    assert os.path.exists(hed_file) is True
+    assert os.path.exists(dos_file) is True
+    logger.info("Checking if output file " + hed_file + " is not empty")
+    assert os.path.getsize(hed_file) > 1
+    logger.info("Checking if output file " + dos_file + " is not empty")
+    assert os.path.getsize(dos_file) > 1
+    os.remove(hed_file)
+    os.remove(dos_file)
--- tests/test_exec.py
+++ tests/test_exec.py
@@ -1,107 +1,88 @@
-# #
-# #    Copyright (C) 2010-2017 PyTRiP98 Developers.
-# #
-# #    This file is part of PyTRiP98.
-# #
-# #    PyTRiP98 is free software: you can redistribute it and/or modify
-# #    it under the terms of the GNU General Public License as published by
-# #    the Free Software Foundation, either version 3 of the License, or
-# #    (at your option) any later version.
-# #
-# #    PyTRiP98 is distributed in the hope that it will be useful,
-# #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-# #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# #    GNU General Public License for more details.
-# #
-# #    You should have received a copy of the GNU General Public License
-# #    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
-# #
-# """
-# TODO: documentation here.
-# """
-# import os
-# import unittest
-# import logging
 #
-# import pytrip as pt
-# import pytrip.tripexecuter as pte
+#    Copyright (C) 2010-2017 PyTRiP98 Developers.
 #
-# import tests.base
+#    This file is part of PyTRiP98.
 #
-# logger = logging.getLogger(__name__)
+#    PyTRiP98 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
+#    PyTRiP98 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-# class TestLocalExec(unittest.TestCase):
-#     """ Tests for pytrip.tripexecuter
-#     """
-#     def setUp(self):
-#         """ Prepare test environment.
-#         """
-#         testdir = tests.base.get_files()
+#    You should have received a copy of the GNU General Public License
+#    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
 #
-#         self.patient_name = "tst003000"
-#
-#         self.ctx_path = os.path.join(testdir, self.patient_name + '.ctx')
-#         self.vdx_path = os.path.join(testdir, self.patient_name + '.vdx')
-#         self.trip_path = os.path.join(testdir, "TRiP98")
-#
-#     def test_exec(self):
-#         """ Prepare and execute a dry-run test using the Executer.
-#         """
-#         logger.info("Test norun TRiP98 execution")
-#
-#         logger.debug("Load CtxCube {:s}".format(self.ctx_path))
-#         c = pt.CtxCube()
-#         c.read(self.ctx_path)
-#
-#         logger.debug("Load VdxCube {:s}".format(self.vdx_path))
-#         v = pt.VdxCube(c)
-#         v.read(self.vdx_path)
-#
-#         print(v.voi_names())
-#
-#         projectile = pte.Projectile('H')
-#         kernel = pte.KernelModel(projectile)
-#         kernel.ddd_path = "/opt/TRiP98/base/DATA/DDD/12C/RF3MM/*"
-#         kernel.spc_path = "/opt/TRiP98/base/DATA/SPC/12C/RF3MM/*"
-#         kernel.sis_path = "/opt/TRiP98/base/DATA/SIS/12C.sis"
-#         kernel.rifi_thickness = 3.0
-#         plan = pte.Plan(default_kernel=kernel, basename=self.patient_name)
-#         self.assertIsNotNone(plan)
-#
-#         plan.hlut_path = "/opt/TRiP98/base/DATA/HLUT/19990218.hlut"
-#         plan.dedx_path = "/opt/TRiP98/base/DATA/DEDX/20040607.dedx"
-#         plan.working_dir = "."  # working dir must exist.
-#
-#         # add the target voi to the plan
-#         plan.voi_target = v.get_voi_by_name('target')
-#
-#         plan.bolus = 0.0
-#         plan.offh2o = 1.873
-#
-#         # create a field and add it to the plan
-#         field = pte.Field(kernel)
-#         self.assertIsNotNone(field)
-#         field.basename = self.patient_name
-#         field.gantry = 10.0
-#         field.couch = 90.0  # degrees
-#         field.fwhm = 4.0  # spot size in [mm]
-#
-#         plan.fields.append(field)
-#
-#         # flags for what output should be generated
-#         plan.want_phys_dose = True
-#         plan.want_bio_dose = False
-#         plan.want_dlet = True
-#         plan.want_rst = False
-#
-#         t = pte.Execute(c, v)
-#         self.assertIsNotNone(t)
-#         t.trip_bin_path = self.trip_path
-#         print(self.trip_path)
-#         if os.name != 'nt':  # skip running fake TRiP98 on Windows as it is not supported there
-#             t.execute(plan, False)  # setup and make a dry-run, since TRiP98 is not installed.
-#
-#         executer_str = str(t)
-#         self.assertGreater(len(executer_str), 1)
-#         # No results will be generated since, TRiP98 is not installed in test environment.
+"""
+TODO: documentation here.
+"""
+import logging
+import os
+
+import pytrip as pt
+import pytrip.tripexecuter as pte
+
+logger = logging.getLogger(__name__)
+
+
+def test_exec(ctx_corename, vdx_filename, patient_name="tst003000"):
+    """Prepare and execute a dry-run test using the Executer."""
+    logger.info("Test norun TRiP98 execution")
+
+    logger.debug("Load CtxCube {:s}".format(ctx_corename))
+    c = pt.CtxCube()
+    c.read(ctx_corename)
+
+    logger.debug("Load VdxCube {:s}".format(vdx_filename))
+    v = pt.VdxCube(c)
+    v.read(vdx_filename)
+
+    print(v.voi_names())
+
+    projectile = pte.Projectile('H')
+    kernel = pte.KernelModel(projectile)
+    kernel.ddd_path = "/opt/TRiP98/base/DATA/DDD/12C/RF3MM/*"
+    kernel.spc_path = "/opt/TRiP98/base/DATA/SPC/12C/RF3MM/*"
+    kernel.sis_path = "/opt/TRiP98/base/DATA/SIS/12C.sis"
+    kernel.rifi_thickness = 3.0
+    plan = pte.Plan(default_kernel=kernel, basename=patient_name)
+    assert plan is not None
+
+    plan.hlut_path = "/opt/TRiP98/base/DATA/HLUT/19990218.hlut"
+    plan.dedx_path = "/opt/TRiP98/base/DATA/DEDX/20040607.dedx"
+    plan.working_dir = "."  # working dir must exist.
+
+    # add the target voi to the plan
+    plan.voi_target = v.get_voi_by_name('target')
+
+    plan.bolus = 0.0
+    plan.offh2o = 1.873
+
+    # create a field and add it to the plan
+    field = pte.Field(kernel)
+    assert field is not None
+    field.basename = patient_name
+    field.gantry = 10.0
+    field.couch = 90.0  # degrees
+    field.fwhm = 4.0  # spot size in [mm]
+
+    plan.fields.append(field)
+
+    # flags for what output should be generated
+    plan.want_phys_dose = True
+    plan.want_bio_dose = False
+    plan.want_dlet = True
+    plan.want_rst = False
+
+    t = pte.Execute(c, v)
+    assert t is not None
+    t.trip_bin_path = '/dev/null'
+    if os.name != 'nt':  # skip running fake TRiP98 on Windows as it is not supported there
+        t.execute(plan, False)  # setup and make a dry-run, since TRiP98 is not installed.
+
+    executer_str = str(t)
+    assert len(executer_str) > 1
+    # No results will be generated since, TRiP98 is not installed in test environment.
