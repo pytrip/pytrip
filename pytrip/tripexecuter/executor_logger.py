56a88b672ca4abf9011e40e66e7c13dd07a21709
Leszek Grzanka <grzanka@agh.edu.pl>
2021-11-20 22:12:24 +0000

    trigger full [build]

--- pytrip/tripexecuter/executor_logger.py
+++ pytrip/tripexecuter/executor_logger.py
@@ -16,9 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with PyTRiP98.  If not, see <http://www.gnu.org/licenses/>.
 #
-"""
-This file contains the ExecutorLogger classes which are used to log execution logs
-"""
+"""This file contains the ExecutorLogger classes which are used to log execution logs"""
 
 import logging
 
