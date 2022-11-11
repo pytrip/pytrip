f2096608608698dee43f16d9bf4ac95711bfaf32
Leszek Grzanka <grzanka@agh.edu.pl>
2022-11-11 11:18:42 +0000

    comma added [build]

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ install_requires = [
     "matplotlib; python_version > '3.5'",
     "matplotlib<3.1 ; python_version <= '3.5'",
     "pydicom @ git+https://github.com/pydicom/pydicom.git@a8be738418dee0a2b93c241fbd5e0bc82f4b8680 ; python_version == '3.11'",
-    "pydicom ; python_version <= '3.11'"
+    "pydicom ; python_version <= '3.11'",
     "scipy ; python_version > '3.5'",
     "scipy<1.3 ; python_version <= '3.5'",
     "kiwisolver==1.1 ; python_version <= '3.5'",
