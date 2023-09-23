28cf91a84f37357d8168df49d9012e8d1195415a
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-09-23 12:04:32 +0200

    dropping support for python 2.7

--- setup.py
+++ setup.py
@@ -220,7 +220,6 @@ setuptools.setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: C',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -248,4 +247,4 @@ setuptools.setup(
             'spc2pdf=pytrip.utils.spc2pdf:main',
         ],
     },
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.3.*, !=3.5.*')
+    python_requires='>=3.6)
